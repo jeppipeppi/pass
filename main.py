921bab52e8bbaa5ce7f7c1bcb7bcc863bf842177
Jeppi <jesper.fuella2@gmail.com>
2022-11-17 16:53:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,172 @@
+import random
+
+digits = ["1", "2", "3", "4"]
+numberlist = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+upperletter = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+underletter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+stuff = ["!", "§", "$", "%", "&", "/", "(", ")", "=", "?"]
+
+
+
+digit1 = random.choice(digits)
+
+if digit1 == "1":
+    dig1 = random.choice(numberlist)
+
+if digit1 == "2":
+    dig1 = random.choice(upperletter)
+
+if digit1 == "3":
+    dig1 = random.choice(underletter)
+
+if digit1 == "4":
+    dig1 = random.choice(stuff)
+
+
+
+digit2 = random.choice(digits)
+
+if digit2 == "1":
+    dig2 = random.choice(numberlist)
+
+if digit2 == "2":
+    dig2 = random.choice(upperletter)
+
+if digit2 == "3":
+    dig2 = random.choice(underletter)
+
+if digit2 == "4":
+    dig2 = random.choice(stuff)
+
+
+
+digit3 = random.choice(digits)
+
+if digit3 == "1":
+    dig3 = random.choice(numberlist)
+
+if digit3 == "2":
+    dig3 = random.choice(upperletter)
+
+if digit3 == "3":
+    dig3 = random.choice(underletter)
+
+if digit3 == "4":
+    dig3 = random.choice(stuff)
+
+
+
+digit4 = random.choice(digits)
+
+if digit4 == "1":
+    dig4 = random.choice(numberlist)
+
+if digit4 == "2":
+    dig4 = random.choice(upperletter)
+
+if digit4 == "3":
+    dig4 = random.choice(underletter)
+
+if digit4 == "4":
+    dig4 = random.choice(stuff)
+
+
+digit5 = random.choice(digits)
+
+if digit5 == "1":
+    dig5 = random.choice(numberlist)
+
+if digit5 == "2":
+    dig5 = random.choice(upperletter)
+
+if digit5 == "3":
+    dig5 = random.choice(underletter)
+
+if digit5 == "4":
+    dig5 = random.choice(stuff)
+
+
+
+
+
+digit6 = random.choice(digits)
+
+if digit6 == "1":
+    dig6 = random.choice(numberlist)
+
+if digit6 == "2":
+    dig6 = random.choice(upperletter)
+
+if digit6 == "3":
+    dig6 = random.choice(underletter)
+
+if digit6 == "4":
+    dig6 = random.choice(stuff)
+
+
+
+
+digit7 = random.choice(digits)
+
+if digit7 == "1":
+    dig7 = random.choice(numberlist)
+
+if digit7 == "2":
+    dig7 = random.choice(upperletter)
+
+if digit7 == "3":
+    dig7 = random.choice(underletter)
+
+if digit7 == "4":
+    dig7 = random.choice(stuff)
+
+
+
+digit8 = random.choice(digits)
+
+if digit8 == "1":
+    dig8 = random.choice(numberlist)
+
+if digit8 == "2":
+    dig8 = random.choice(upperletter)
+
+if digit8 == "3":
+    dig8 = random.choice(underletter)
+
+if digit8 == "4":
+    dig8 = random.choice(stuff)
+
+
+digit9 = random.choice(digits)
+
+if digit9 == "1":
+    dig9 = random.choice(numberlist)
+
+if digit9 == "2":
+    dig9 = random.choice(upperletter)
+
+if digit9 == "3":
+    dig9 = random.choice(underletter)
+
+if digit9 == "4":
+    dig9 = random.choice(stuff)
+
+
+
+
+digit10 = random.choice(digits)
+
+if digit10 == "1":
+    dig10 = random.choice(numberlist)
+
+if digit10 == "2":
+    dig10 = random.choice(upperletter)
+
+if digit10 == "3":
+    dig10 = random.choice(underletter)
+
+if digit10 == "4":
+    dig10 = random.choice(stuff)
+
+
+print(dig1 + dig2 + dig3 + dig4 + dig5 + dig6 + dig7 + dig8 + dig9 + dig10)
